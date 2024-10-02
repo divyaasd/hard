2b51a40daabb9e529a24d6423379be550954c8cf
Divya <divyaas340@gmail.com>
2024-10-02 17:30:28 +0530

    first commit7

--- class/class/settings.py
+++ class/class/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'class.myapp',
+    'class.myapp.apps.MyappConfig',
 ]
 
 MIDDLEWARE = [
